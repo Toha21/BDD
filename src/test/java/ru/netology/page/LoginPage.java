527b66b8ee752a49e672b30962877185c4b5dd46
Антон Гаврюхин <ah21@mail.ru>
2022-10-01 13:15:32 +0300

    add page

---
+++ src/test/java/ru/netology/page/LoginPage.java
@@ -0,0 +1,17 @@
+package ru.netology.page;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import ru.netology.data.DataHelper;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$x;
+
+public class LoginPage {
+    public VerificationPage validLogin(DataHelper.AuthInfo info) {
+        $("[data-test-id=login] input").setValue(info.getLogin());
+        $("[data-test-id=password] input").setValue(info.getPassword());
+        $("[data-test-id=action-login]").click();
+        return new VerificationPage();
+    }
+}
---
+++ src/test/java/ru/netology/page/TransferPage.java
@@ -0,0 +1,26 @@
+package ru.netology.page;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+
+public class TransferPage {
+    private SelenideElement amountField = $("[data-test-id=amount] input");
+    private SelenideElement fromField = $("[data-test-id=from] input");
+    private SelenideElement transferButton = $("[data-test-id=action-transfer]");
+
+    private SelenideElement errorNotification = $("[data-test-id=error-notification]");
+
+    public DashboardPage Transfer(int amount, String cardNumber) {
+        amountField.setValue(Integer.toString(amount));
+        fromField.setValue(cardNumber);
+        transferButton.click();
+        return new DashboardPage();
+    }
+
+    public SelenideElement getNotification() {
+        return errorNotification;
+    }
+}
